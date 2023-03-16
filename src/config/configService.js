e998b01d4f2465ec999abe990d8da28065a75679
AlexAllGold <alexallgold@gmail.com>
2023-03-17 00:35:48 +0200

    fix: added varible into calling constructor InternalServerException

--- src/config/configService.js
+++ src/config/configService.js
@@ -26,7 +26,7 @@ class ConfigService {
     if (process.env[nameEnv]) {
       return process.env[nameEnv];
     }
-    throw new InternalServerException('Env does not exist');
+    throw new InternalServerException(`Env ${nameEnv} does not exist`);
   }
 }
 export const configService = new ConfigService();
