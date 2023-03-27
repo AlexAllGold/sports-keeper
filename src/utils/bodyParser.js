648318c90456e075ab6a1bc164f873361c608ae2
AlexAllGold <alexallgold@gmail.com>
2023-03-28 01:17:44 +0300

    fix: deleted 'try catch' and fix reject

--- src/utils/bodyParser.js
+++ src/utils/bodyParser.js
@@ -2,19 +2,16 @@ class BodyParser {
   async parser(request) {
     let body = [];
     return new Promise((resolve, reject) => {
-      try {
-        request
-          .on('data', (chunk) => {
-            body.push(chunk);
-          })
-          .on('end', () => {
-            body = Buffer.concat(body).toString();
-          });
-        resolve(body);
-      } catch (err) {
-        reject(err);
-      }
+      request
+        .on('error', (error) => {
+          reject(error);
+        })
+        .on('end', () => {
+          body = Buffer.concat(body).toString();
+        });
+      resolve(body);
     });
   }
 }
+
 export const bodyParser = new BodyParser();
