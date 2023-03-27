1da2c467d7b8366afb84e25c942b34f6cd2893ea
AlexAllGold <alexallgold@gmail.com>
2023-03-28 00:59:37 +0300

    refactor: moved all functions from client.dto.js to validator.dto.js

--- src/dtos/сlient.dto.js
+++ src/dtos/сlient.dto.js
@@ -13,28 +13,10 @@ export class ClientDto extends ValidatorDto {
 
   constructor(client) {
     super();
-    this.fillingClient(client);
-  }
-
-  #addName(name) {
-    if (this.lastName.length >= 4 && this.lastName <= 50) {
-      return this.isString(name);
-    }
-    throw new Error('Invalid first name or last name');
-  }
-
-  #addEmail(email) {
-    if (this.email.length >= 3 && this.email <= 256) {
-      return this.isString(email);
-    }
-    throw new Error('Invalid email');
-  }
-
-  fillingClient(client) {
     this.clubId = this.isNumber(client.clubId);
-    this.firstName = this.#addName(client.firstName);
-    this.lastName = this.#addName(client.lastName);
+    this.firstName = this.isName(client.firstName);
+    this.lastName = this.isName(client.lastName);
     this.dateOfBirth = this.isDate(client.dateOfBirth);
-    this.email = this.#addEmail(client.email);
+    this.email = this.isEmail(client.email);
   }
 }
--- src/utils/validator.dto.js
+++ src/utils/validator.dto.js
@@ -1,17 +1,16 @@
 export class ValidatorDto {
+  isName(name) {
+    if (this.#isMin(name) && this.#isMax(name)) {
+      return name;
+    }
+    throw new Error('Invalid first name or last name');
+  }
+
   isNumber(number) {
     if (!Number.isNaN(Number(number))) {
       return number;
     }
     throw new Error('Not a number');
-  }
-
-  isString(firstName) {
-    try {
-      return String(firstName);
-    } catch (error) {
-      throw new Error('Invalid first name');
-    }
   }
 
   isDate(dateOfBirth) {
@@ -20,5 +19,26 @@ export class ValidatorDto {
       return new Date(date);
     }
     throw new Error('Invalid of date of birth');
+  }
+
+  isEmail(email) {
+    if (/^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/.test(email)) {
+      return email;
+    }
+    throw new Error('Invalid email address');
+  }
+
+  #isMin(min) {
+    if (min.length >= 4) {
+      return true;
+    }
+    throw new Error('Invalid min value');
+  }
+
+  #isMax(max) {
+    if (max.length <= 50) {
+      return true;
+    }
+    throw new Error('Invalid maximum value');
   }
 }
