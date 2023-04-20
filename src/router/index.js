0d9ae497dc4e8f6fc44accbfbcaa786397582b86
AlexAllGold <alexallgold@gmail.com>
2023-04-21 01:44:37 +0300

    fix: changed route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { routes } from './appRoutes.js';
 import { sendResponse } from '../utils/sendResponse.js';
 import { httpCodes } from '../utils/httpCodes.js';
+import { NotFoundException } from '../utils/exceptions/NotFoundException.js';
 
 export const router = (req, res) => {
   const params = {};
@@ -21,19 +22,49 @@ export const router = (req, res) => {
     return resultPath;
   };
 
-  const startRout = (url) => {
+  const getController = async (url) => {
     let flag = false;
     const currentUrl = glueUrl(url, routes[routes.length - 1].path);
-    routes.forEach(async (rout) => {
-      if (rout.path === currentUrl) {
+    routes.forEach((route) => {
+      if (route.path === currentUrl) {
+        route.methods[req.method](res, req, params);
         flag = true;
-        await rout.methods[req.method](res, req, params);
       }
     });
-    if (!flag) {
-      sendResponse(res, httpCodes.NOT_FOUND, 'NotFound');
+    return flag;
+  };
+
+  const startRout = async (url) => {
+    try {
+      const foundRoute = await getController(url);
+      if (!foundRoute) {
+        throw new NotFoundException('No route found');
+      }
+    } catch (err) {
+      sendResponse(res, httpCodes.INTERNAL_SERVER_ERROR, 'Something went wrong');
     }
   };
 
   startRout(req.url);
 };
+// const startRout = (url) => {
+//   let flag = false;
+//   const currentUrl = glueUrl(url, routes[routes.length - 1].path);
+//   routes.forEach(async (rout) => {
+//     if (rout.path === currentUrl) {
+//       flag = true;
+//       await rout.methods[req.method](res, req, params);
+//     }
+//   });
+//   if (!flag) {
+//     sendResponse(res, httpCodes.NOT_FOUND, 'NotFound');
+//   }
+// };
+// try {
+//   const foundController = getController(req);
+//   if (!foundController) {
+//     throw new NotFoundException(“Url ${req.url} not found”);
+//     await foundController(req, res);
+//   } catch(err) {
+//     sendResponse(res, HttpCode.INTERNAL_SERVER_ERROR, “Something went wrong”);
+//   }
