e7f724357b3847538623d57b2ba097439e442f00
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2023-11-14 23:02:41 +0200

    message

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,13 +1,13 @@
 import { validateOrReject } from 'class-validator';
-import type { NextFunction, Response, Request } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import { BadRequestException } from '../utils/exceptions/BadRequestException';
+import { CreateClubDto } from '../dtos/createClub.dto';
 
-export const validationMiddleware =
-  (Dto) =>
-  (req: Request, res: Response, next: NextFunction): void => {
-    validateOrReject(new Dto(req.body))
-      .then(next)
-      .catch((error) => {
-        next(new BadRequestException(error as string));
-      });
-  };
+export const validationMiddleware = (Dto: CreateClubDto) => (req: Request, res: Response, next: NextFunction): void => {
+const dto = new Dto(req.body) as unknown as CreateClubDto;
+    validateOrReject(dto)
+        .then(next)
+        .catch((error) => {
+            next(new BadRequestException(error as string));
+        });
+};
