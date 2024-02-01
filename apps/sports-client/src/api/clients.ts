97bc5fb4e237c09d3c712157c23cec25de9fc543
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2024-02-02 00:14:06 +0200

    feat: added toast for created;

--- apps/sports-client/src/api/clients.ts
+++ apps/sports-client/src/api/clients.ts
@@ -4,70 +4,55 @@ import { toast } from 'react-toastify';
 import { CreateClient, IClient } from '../models/IClient';
 import 'react-toastify/dist/ReactToastify.css';
 
-
-const clientsApiAxios = axios.create({baseURL: 'http://localhost:8000/api/clubs/'})
+const clientsApiAxios = axios.create({ baseURL: 'http://localhost:8000/api/clubs/' });
 clientsApiAxios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status < 500 && error.response.status > 300) {
-      toast.warning(error.response.data)
-    } else {
-      toast.error(error.response.data);
-    }
-  }
-)
-export const fetchAllClients = createAsyncThunk(
-  'client/fetchAll',
-  async (clubId: string | undefined) => {
-      const response = await clientsApiAxios.get<IClient[]>(`${clubId}/clients`);
-      return response.data;
-  },
+	(response) => response,
+	(error) => {
+		if (error.response.status < 401 && error.response.status > 300) {
+			toast.warning(error.response.data);
+		} else {
+			toast.error(error.response.data);
+		}
+	},
 );
+export const fetchAllClients = createAsyncThunk('client/fetchAll', async (clubId: string | undefined) => {
+	const response = await clientsApiAxios.get<IClient[]>(`${clubId}/clients`);
+	return response.data;
+});
 
-export const fetchClient = createAsyncThunk(
-  'client/fetchOne',
-  async (client: IClient) => {
-    const {id, clubId} = client;
-      const response = await clientsApiAxios.get<IClient>(`${clubId}/clients/${id}`);
-      return response.data;
-  },
-);
+export const fetchClient = createAsyncThunk('client/fetchOne', async (client: IClient) => {
+	const { id, clubId } = client;
+	const response = await clientsApiAxios.get<IClient>(`${clubId}/clients/${id}`);
+	return response.data;
+});
 
-export const createClient = createAsyncThunk(
-  'client/createClient',
-  async (client: CreateClient) => {
-    const { firstName, lastName, email, clubId, dateOfBirth } = client;
-      const response = await clientsApiAxios.post<CreateClient>(`${clubId}/clients`, {
-        firstName,
-        lastName,
-        email,
-        clubId,
-        dateOfBirth,
-      });
-      return response.data;
-  },
-);
+export const createClient = createAsyncThunk('client/createClient', async (client: CreateClient) => {
+	const { firstName, lastName, email, clubId, dateOfBirth } = client;
+	const response = await clientsApiAxios.post<CreateClient>(`${clubId}/clients`, {
+		firstName,
+		lastName,
+		email,
+		clubId,
+		dateOfBirth,
+	});
+	toast.success('Success!');
+	return response.data;
+});
 
-export const updateClient = createAsyncThunk(
-  'client/updateClient',
-  async (client: IClient) => {
-    const { id, firstName, lastName, email, clubId, dateOfBirth } = client;
-      const response = await clientsApiAxios.put<IClient>(`${clubId}/clients/${id}`, {
-        id,
-        clubId,
-        firstName,
-        lastName,
-        email,
-        dateOfBirth,
-      });
-      return response.data;
-  },
-);
-export const removeClient = createAsyncThunk(
-  'client/remove',
-  async (client: IClient) => {
-    const { clubId, id } = client;
-      const response = await clientsApiAxios.delete<IClient>(`${clubId}/clients/${id}`);
-      return response.data ? response.data : toast.success('Client Успешно Удален!', {});
-  },
-);
+export const updateClient = createAsyncThunk('client/updateClient', async (client: IClient) => {
+	const { id, firstName, lastName, email, clubId, dateOfBirth } = client;
+	const response = await clientsApiAxios.put<IClient>(`${clubId}/clients/${id}`, {
+		id,
+		clubId,
+		firstName,
+		lastName,
+		email,
+		dateOfBirth,
+	});
+	return response.data;
+});
+export const removeClient = createAsyncThunk('client/remove', async (client: IClient) => {
+	const { clubId, id } = client;
+	const response = await clientsApiAxios.delete<IClient>(`${clubId}/clients/${id}`);
+	return response.data ? response.data : toast.success('Success Deleted!', {});
+});
--- apps/sports-client/src/api/clubs.ts
+++ apps/sports-client/src/api/clubs.ts
@@ -4,66 +4,51 @@ import { toast } from 'react-toastify';
 import { CreateClub, IClub } from '../models/IClub';
 import 'react-toastify/dist/ReactToastify.css';
 
-const clubsApiAxios = axios.create({baseURL: 'http://localhost:8000/api/clubs'})
+const clubsApiAxios = axios.create({ baseURL: 'http://localhost:8000/api/clubs' });
 clubsApiAxios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status < 500 && error.response.status > 300) {
-      toast.warning(error.response.data)
-    } else {
-      toast.error(error.response.data);
-    }
-  }
-)
+	(response) => response,
+	(error) => {
+		if (error.response.status < 401 && error.response.status > 300) {
+			toast.warning(error.response.data);
+		} else {
+			toast.error(error.response.data);
+		}
+	},
+);
 
+export const fetchAllClubs = createAsyncThunk('club/fetchAll', async (_) => {
+	const response = await clubsApiAxios.get<IClub[]>('');
+	return response.data;
+});
 
-export const fetchAllClubs = createAsyncThunk(
-  'club/fetchAll',
-   async (_) => {
-      const response = await clubsApiAxios.get<IClub[]>('')
-      return response.data
-    }
-)
+export const fetchClub = createAsyncThunk('club/fetchOne', async (id: string | undefined) => {
+	const response = await clubsApiAxios.get<IClub>(`/${id}`);
+	return response.data;
+});
 
-export const fetchClub = createAsyncThunk(
-  'club/fetchOne',
-  async (id: string | undefined) => {
-      const response = await clubsApiAxios.get<IClub>(`/${id}`)
-      return response.data
-  }
-)
+export const createClub = createAsyncThunk('club/createClub', async (club: CreateClub) => {
+	const { name, description, address } = club;
+	const response = await clubsApiAxios.post<CreateClub>('', {
+		name,
+		description,
+		address,
+	});
+	toast.success('Success!');
+	return response.data;
+});
 
-export const createClub = createAsyncThunk(
-  'club/createClub',
-  async (club: CreateClub) => {
-    const {name, description, address} = club;
-      const response = await clubsApiAxios.post<CreateClub>('',  {
-        name,
-        description,
-        address,
-      })
-      return response.data
-  }
-)
-
-export const updateClub = createAsyncThunk(
-  'club/updateClub',
-  async (club: IClub) => {
-    const {id, name, description, address} = club;
-      const response = await clubsApiAxios.put<IClub>(`/${id}`, {
-        id,
-        name,
-        description,
-        address,
-      })
-      return response.data
-    }
-)
-export const removeClub = createAsyncThunk(
-  'club/remove',
-  async (id: string) => {
-      const response = await clubsApiAxios.delete<IClub>(`/${id}`)
-      toast.success('Клуб Успешно Удален!', {});
-      return response.data
-    }
-)
+export const updateClub = createAsyncThunk('club/updateClub', async (club: IClub) => {
+	const { id, name, description, address } = club;
+	const response = await clubsApiAxios.put<IClub>(`/${id}`, {
+		id,
+		name,
+		description,
+		address,
+	});
+	return response.data;
+});
+export const removeClub = createAsyncThunk('club/remove', async (id: string) => {
+	const response = await clubsApiAxios.delete<IClub>(`/${id}`);
+	toast.success('Success Deleted!', {});
+	return response.data;
+});
