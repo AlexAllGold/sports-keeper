270ae4773086f1f7d1101d4995311e9f5432024d
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2024-02-06 23:04:36 +0200

    feat: added header and button back;

--- apps/sports-client/src/app/layouts/Layout.navigation.bar.tsx
+++ apps/sports-client/src/app/layouts/Layout.navigation.bar.tsx
@@ -2,19 +2,23 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Bar } from '../components/bar/Bar';
+import Header from '../components/Header';
 
 function LayoutNavigationBar() {
-  return (
-    <div className="flex h-full w-full rounded-xl bg-white">
-      <Bar />
-      <div className='flex flex-col w-full px-6 gap-4'>
-        <React.Suspense fallback='Loading...'>
-          <Outlet />
-        </React.Suspense>
-      </div>
-      <ToastContainer/>
-    </div>
-  );
+	return (
+		<div className="flex h-full w-full rounded-xl bg-white">
+			<div className="flex">
+				<Bar />
+			</div>
+			<div className="flex flex-col p-6 gap-4">
+				<React.Suspense fallback="Loading...">
+					<Header />
+					<Outlet />
+				</React.Suspense>
+			</div>
+			<ToastContainer />
+		</div>
+	);
 }
 
 export { LayoutNavigationBar };
