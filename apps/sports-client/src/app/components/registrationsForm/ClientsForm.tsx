fc6992fca9d8e9411d7ac5760a2d4e5401e61e6a
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2024-02-10 23:49:23 +0200

    feat: datepicker;

--- apps/sports-client/src/app/components/registrationsForm/ClientsForm.tsx
+++ apps/sports-client/src/app/components/registrationsForm/ClientsForm.tsx
@@ -1,17 +1,23 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { SubmitHandler } from 'react-hook-form';
+import { SubmitHandler, Controller } from 'react-hook-form';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import uk from 'date-fns/locale/uk';
 import { createClient, fetchClient, updateClient } from '../../../api/clients';
 import { useAppDispatch } from '../../../hooks/redux';
 import { CreateClient } from '../../../models/IClient';
 import { useClientForm } from '../../../hooks/useClientForm';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 export function ClientsForm() {
+	registerLocale('uk', uk);
 	const { clubId, clientId: id } = useParams();
 	const idAsNumber = parseInt(id as string, 10);
 	const dispatch = useAppDispatch();
 	const navigate = useNavigate();
 	const {
+		control,
 		register,
 		handleSubmit,
 		setValue,
@@ -40,7 +46,6 @@ export function ClientsForm() {
 	return (
 		<div className="flex flex-col h-full  pb-10 px-32">
 			<h1 className="flex m-2 justify-center text-3xl font-medium">{id ? 'Edit' : 'Register'} Client</h1>
-
 			<form onSubmit={handleSubmit(save)} className="flex flex-col w-full gap-0.5 py-12 m-auto border border-[#CAD0D8] rounded-xl">
 				<div className="flex flex-row justify-between px-10">
 					<div className="flex flex-col">
@@ -65,11 +70,24 @@ export function ClientsForm() {
 
 					<div className="flex-col">
 						<p className="pl-2 text-blue-600">ID</p>
-						<input readOnly className="Input-style " {...register('clubId')} />
+						<input readOnly className="Input-style" {...register('clubId')} />
 						<p className="text-center text-red-600">{errors.clubId?.message}</p>
 
 						<p className="pl-2 text-blue-600">Date of Birthday</p>
-						<input className="Input-style" {...register('dateOfBirth')} />
+						<Controller
+							render={({ field }) => (
+								<DatePicker
+									{...field}
+									selected={field.value}
+									onChange={(date: Date) => field.onChange(date)}
+									name="dateOfBirth"
+									className="Input-style"
+									dateFormat="dd/MM/yyyy" // Установите нужный формат даты
+								/>
+							)}
+							control={control}
+							name="dateOfBirth"
+						/>
 						<p className="text-center text-red-600">{errors.dateOfBirth?.message}</p>
 					</div>
 				</div>
